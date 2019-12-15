e4b7fe2be91c3cd65f6878e1d4c0419942c290b4
Harshal Sheth <hsheth2@gmail.com>
2019-12-15 15:01:38 -0500

    Initial outlier detection

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import spotipy
 import spotipy.util
 import dotenv
 import pprint
+import numpy as np
+from pyod.models.knn import KNN
 
 import data
 
@@ -20,10 +22,60 @@ if not token:
 sp = spotipy.Spotify(auth=token)
 
 
-playlist_id = "https://open.spotify.com/playlist/3z91HHZMlFJsUZquZBbQnX"
+def track_characteristics_array(sp: spotipy.client.Spotify, track_uri: str):
+    # An example response from the API:
+    """
+    {'acousticness': 0.0422,
+    'analysis_url': 'https://api.spotify.com/v1/audio-analysis/2cGxRwrMyEAp8dEbuZaVv6',
+    'danceability': 0.775,
+    'duration_ms': 337560,
+    'energy': 0.585,
+    'id': '2cGxRwrMyEAp8dEbuZaVv6',
+    'instrumentalness': 0.619,
+    'key': 10,
+    'liveness': 0.077,
+    'loudness': -9.516,
+    'mode': 0,
+    'speechiness': 0.0271,
+    'tempo': 109.942,
+    'time_signature': 4,
+    'track_href': 'https://api.spotify.com/v1/tracks/2cGxRwrMyEAp8dEbuZaVv6',
+    'type': 'audio_features',
+    'uri': 'spotify:track:2cGxRwrMyEAp8dEbuZaVv6',
+    'valence': 0.518}
+    """
 
+    characteristics = [
+        'acousticness',
+        'danceability',
+        'duration_ms',
+        'energy',
+        'instrumentalness',
+        #'key',  # Categorical: pitch class notation
+        'liveness',
+        'loudness',
+        'mode',  # Categorical: major or minor
+        'speechiness',
+        'tempo',
+        'time_signature',
+        'valence',
+    ]
+
+    track = data.get_audio_features(sp, track_uri)
+
+    values = [track[feature] for feature in characteristics]
+    return values
+
+playlist_id = "https://open.spotify.com/playlist/3z91HHZMlFJsUZquZBbQnX"
 uris = data.get_playlist(sp, username, playlist_id)
 
 # print("   %d %32.32s %s" % (i, track["artists"][0]["name"], track["name"]))
-# info = data.get_audio_features(sp, track)
-# pprint.pprint(info)
+
+# uri = 'spotify:track:2cGxRwrMyEAp8dEbuZaVv6'
+# info = data.get_audio_features(sp, uri)
+# values = track_characteristics_array(sp, uri)
+
+X = np.array([track_characteristics_array(sp, track_uri) for track_uri in uris])
+
+clf = KNN()
+clf.fit(X)
