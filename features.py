bf186e97a5b99f809262b38e11e76b44801f72b8
Harshal Sheth <hsheth2@gmail.com>
2019-12-14 20:35:18 -0500

    Format with black

--- features.py
+++ features.py
@@ -2,19 +2,20 @@ import json
 import spotipy
 import os
 
-def get_audio_features(sp: spotipy.client.Spotify, track_uri: str):
-    track_uri = sp._get_id('track', track_uri)
 
-    os.mkdir('audio_features')
-    filename = f'audio_features/{track_uri}'
+def get_audio_features(sp: spotipy.client.Spotify, track_uri: str):
+    track_uri = sp._get_id("track", track_uri)
+
+    os.mkdir("audio_features")
+    filename = f"audio_features/{track_uri}"
     try:
-        with open(filename, 'r') as f:
+        with open(filename, "r") as f:
             contents = f.read()
             data = json.loads(contents)
     except FileNotFoundError:
         features = sp.audio_features([track_uri])
         data = features[0]
-        with open(filename, 'w') as f:
+        with open(filename, "w") as f:
             contents = json.dumps(data, indent=4)
             f.write(contents)
     return data
--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import features
 
 dotenv.load_dotenv()
 
-scope = 'user-library-read,playlist-read-private'
+scope = "user-library-read,playlist-read-private"
 
-username = 'hsheth2'
+username = "hsheth2"
 
 token = spotipy.util.prompt_for_user_token(username, scope)
 if not token:
@@ -19,27 +19,32 @@ if not token:
 
 sp = spotipy.Spotify(auth=token)
 
+
 def fetch_tracks_from_playlist(sp, username, playlist_id):
     all_tracks = []
     results = sp.user_playlist(username, playlist_id, fields="tracks,next")
 
-    tracks = results['tracks']
-    all_tracks += tracks['items']
-    while tracks['next']:
+    tracks = results["tracks"]
+    all_tracks += tracks["items"]
+    while tracks["next"]:
         tracks = sp.next(tracks)
-        all_tracks += tracks['items']
-    
-    return [item['track'] for item in all_tracks]
+        all_tracks += tracks["items"]
+
+    return [item["track"] for item in all_tracks]
+
 
 def show_tracks(tracks):
     for i, track in enumerate(tracks):
-        print("   %d %32.32s %s" % (i, track['artists'][0]['name'], track['name']))
+        print("   %d %32.32s %s" % (i, track["artists"][0]["name"], track["name"]))
 
-playlist_id = 'https://open.spotify.com/playlist/3z91HHZMlFJsUZquZBbQnX?si=QfnqrFj4SoC6B5rZDdF07A'
+
+playlist_id = (
+    "https://open.spotify.com/playlist/3z91HHZMlFJsUZquZBbQnX"
+)
 
 tracks = fetch_tracks_from_playlist(sp, username, playlist_id)
 show_tracks(tracks)
 pprint.pprint(tracks[0])
 
-#info = features.get_audio_features(sp, track)
-#pprint.pprint(info)
+# info = features.get_audio_features(sp, track)
+# pprint.pprint(info)
